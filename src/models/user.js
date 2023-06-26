095b1313e228e27c5aadf61c73329a828c67e3a7
Gautam karma <goutamkarma97541@gmail.com>
2023-06-26 19:09:23 +0530

    admin/dashboard/functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,69 +1,65 @@
 module.exports = (sequelize, DataTypes) => {
-    const user = sequelize.define(
-        'user',
-        {
-            firstName: {
-                type: DataTypes.STRING(256),
-            },
-            lastName: {
-                type: DataTypes.STRING(256),
-            },
-            email: {
-                type: DataTypes.STRING(256),
-                unique: true
-            },
-            password: {
-                type: DataTypes.STRING,
-            },
-            gender: {
-                type: DataTypes.STRING(256),
-            },
-            phoneNumber: {
-                type: DataTypes.STRING(256),
-                unique: {
-                    args: 'phoneNumber',
-                    msg: 'The phoneNumber is already taken!',
-                },
-            },
-            passwordResetToken: {
-                type: DataTypes.STRING(191),
-            },
-            profileImage: {
-                type: DataTypes.STRING,
-                set(val) {
-                    let tmpStr = val;
-                    tmpStr = tmpStr.replace(/\\/g, '/');
-                    this.setDataValue('profileImage', tmpStr);
-                },
-            },
+  const user = sequelize.define(
+    "user",
+    {
+      firstName: {
+        type: DataTypes.STRING(256),
+      },
+      lastName: {
+        type: DataTypes.STRING(256),
+      },
+      email: {
+        type: DataTypes.STRING(256),
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      gender: {
+        type: DataTypes.STRING(256),
+      },
+      phoneNumber: {
+        type: DataTypes.STRING(256),
+      },
+      passwordResetToken: {
+        type: DataTypes.STRING(191),
+      },
+      profileImage: {
+        type: DataTypes.STRING,
+        set(val) {
+          let tmpStr = val;
+          tmpStr = tmpStr.replace(/\\/g, "/");
+          this.setDataValue("profileImage", tmpStr);
         },
-        {timestamps:false}
-    );
+      },
+    },
+    { timestamps: false }
+  );
 
-    user.addScope('userRole', (data) => ({
+  user.addScope("userRole", (data) => ({
+    include: [
+      {
+        model: sequelize.models.userRole,
+        required: true,
+        attributes: { exclude: ["createdAt", "updatedAt"] },
         include: [
-            {
-                model: sequelize.models.userRole,
-                required: true,
-                attributes: { exclude: ['createdAt', 'updatedAt'] },
-                include: [
-                    {
-                        model: sequelize.models.role,
-                        where: data.whereRole,
-                        required: true,
-                       // attributes: { exclude: ['createdAt', 'updatedAt'] },
-                    },
-                ],
-            },
-            // {
-            //     model: sequelize.models.userAddress,
-            //     attributes: { exclude: ['createdAt', 'updatedAt'] },
-            // },
+          {
+            model: sequelize.models.role,
+            where: data.whereRole,
+            required: true,
+            // attributes: { exclude: ['createdAt', 'updatedAt'] },
+          },
         ],
-    }));
+      },
+      // {
+      //     model: sequelize.models.userAddress,
+      //     attributes: { exclude: ['createdAt', 'updatedAt'] },
+      // },
+    ],
+  }));
 
-    user.associate = (models) => {
-        user.hasOne(models.userRole, { foreignKey: 'userId', onDelete: 'cascade' });
-    }
-    return user;
-}
+  user.associate = (models) => {
+    user.hasOne(models.userRole, { foreignKey: "userId", onDelete: "cascade" });
+  };
+  return user;
+};
