314438c2a2be39f20c01cb4a557e55f3e22ad43e
Gautam karma <goutamkarma97541@gmail.com>
2023-06-28 10:49:49 +0530

    updated myside

--- src/models/doctor.js
+++ src/models/doctor.js
@@ -1,41 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-
-    const doctor = sequelize.define(
-        'doctor',
-        {
-            dateOfBirth: {
-                type: DataTypes.DATEONLY,
-            },
-            address: {
-                type: DataTypes.STRING,
-            },
-            experience: {
-                type: DataTypes.INTEGER,
-            },
-            userId: {
-                type: DataTypes.INTEGER,
-                unique: true
-            },
-        },
-        {
-            timestamps: false,
-            underscored: true
-        }
-
-    );
-    doctor.associate = (models) => {
-        doctor.belongsTo(models.user, { foreignKey: 'user_id' });
-
-        // doctor.hasMany(models.clinic, {
-        //     through:models.d,
-        //     foreignKey: "doctorId",
-        //     onDelete: "cascade"
-        // });
-
+  const doctor = sequelize.define(
+    "doctor",
+    {
+      dateOfBirth: {
+        type: DataTypes.DATEONLY,
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      experience: {
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        unique: true,
+      },
+    },
+    {
+      timestamps: false,
+      underscored: true,
     }
+  );
+  doctor.associate = (models) => {
+    doctor.belongsTo(models.user, { foreignKey: "user_id" });
+  };
 
-
-
-    return doctor;
-
-}
+  return doctor;
+};
